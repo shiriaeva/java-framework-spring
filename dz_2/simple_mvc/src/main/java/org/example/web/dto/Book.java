4c310d2676d84750cf158a41d9609a17b61dfa91
shiriaeva <84004210+shiriaeva@users.noreply.github.com>
2022-10-13 00:02:38 +0500

    преобразование конфигураций в Java-классы

---
+++ dz_2/simple_mvc/src/main/java/org/example/web/dto/Book.java
@@ -0,0 +1,56 @@
+package org.example.web.dto;
+
+public class Book {
+    private String id;
+    private String author;
+    private String title;
+    private Integer size;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", size=" + size +
+                '}';
+    }
+
+    public boolean is_valid() {
+        return !this.author.isEmpty()
+                || !this.title.isEmpty()
+                || this.size != null;
+    }
+}
