8359ed1466047077e852d6b284a8f5a15d0f9af1
shiriaeva <Elena.Shiriaeva@urfu.me>
2022-12-13 00:31:06 +0500

    +dop_works

---
+++ dop_works/task5/spring_course_mvc_hibernate_aop/src/main/java/com/shiryaeva/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,27 @@
+package com.shiryaeva.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.shiryaeva.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(
+            ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        String methodName = methodSignature.getName();
+
+        System.out.println("Start of " + methodName);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targetMethodResult;
+    }
+}
